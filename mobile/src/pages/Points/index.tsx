838d545cd0a736f44e0aac4179a2ec140bf0a684
andrecreppe <andrecrepper@gmail.com>
2020-06-08 09:59:56 -0300

    Day 4 - Map page complete

--- mobile/src/pages/Points/index.tsx
+++ mobile/src/pages/Points/index.tsx
@@ -1,13 +1,79 @@
 import React from 'react';
-import { View, StyleSheet } from 'react-native'
+import { View, StyleSheet, TouchableOpacity, Text, ScrollView, Image } from 'react-native';
+import Constants from 'expo-constants';
+import { Feather as Icon } from '@expo/vector-icons';
+import { useNavigation } from '@react-navigation/native';
+import MapView, { Marker } from 'react-native-maps';
+import { SvgUri } from 'react-native-svg';
 
 const Points = () => {
+  const navigation = useNavigation();
+
+  function handleNavigateBack() {
+    navigation.goBack();
+  }
+
+  function handleNativateToDetail() {
+    navigation.navigate('Detail');
+  }
+
   return (
-    <View />
+    <>
+      <View style={styles.container}>
+        <TouchableOpacity onPress={handleNavigateBack}>
+          <Icon name='arrow-left' size={20} color='#34cb79' />
+        </TouchableOpacity>
+
+        <Text style={styles.title}>
+          Bem vindo!
+        </Text>
+        <Text style={styles.description}>
+          Encontre no mapa um ponto de coleta próximo de você.
+        </Text>
+
+        <View style={styles.mapContainer}>
+          <MapView 
+            style={styles.map}
+            initialRegion={{
+              latitude: -22.3684931,
+              longitude: -49.0618176,
+              latitudeDelta: 0.014,
+              longitudeDelta: 0.014
+            }} 
+          >
+            <Marker
+              style={styles.mapMarker}
+              onPress={handleNativateToDetail}
+              coordinate={{
+                latitude: -22.3684931,
+                longitude: -49.0618176,
+              }} 
+            >
+              <View style={styles.mapMarkerContainer}>
+                <Image style={styles.mapMarkerImage} source={{ uri: '' }} />
+                <Text style={styles.mapMarkerTitle}>Teste</Text>
+              </View>
+            </Marker>
+          </MapView>
+        </View>
+      </View>
+
+      <View style={styles.itemsContainer}>
+        <ScrollView 
+          horizontal 
+          showsHorizontalScrollIndicator={false}
+          contentContainerStyle={{ paddingHorizontal: 20 }}
+        >
+          <TouchableOpacity style={styles.item} onPress={() => {}}>
+            <SvgUri width={42} height={42} uri="http://192.168.100.102:3333/uploads/lampadas.svg" />
+            <Text style={styles.itemTitle}>Lâmpadas</Text>
+          </TouchableOpacity>
+        </ScrollView>
+      </View>
+    </>
   );
 };
 
-/*
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -104,5 +170,5 @@ const styles = StyleSheet.create({
     fontSize: 13,
   },
 });
-*/
+
 export default Points;
--- server/src/controllers/ItemsController.ts
+++ server/src/controllers/ItemsController.ts
@@ -9,7 +9,8 @@ class ItemsController {
             return {
                 id: item.id,
                 name: item.title,
-                image_url: `http://localhost:3333/uploads/${item.image}`
+                //image_url: `http://localhost:3333/uploads/${item.image}` // - web
+                image_url: `http://192.168.100.102:3333/uploads/${item.image}` // - mobile
             }
         }); 
     
