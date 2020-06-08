f5c641b8efeda431ad60bd0a2820279d49e4d21f
andrecreppe <andrecrepper@gmail.com>
2020-06-08 10:19:34 -0300

    Day 4 - detail page done

--- mobile/src/pages/Detail/index.tsx
+++ mobile/src/pages/Detail/index.tsx
@@ -1,9 +1,48 @@
 import React from 'react';
-import { View, StyleSheet } from 'react-native';
+import { View, StyleSheet, TouchableOpacity, Image, Text, SafeAreaView } from 'react-native';
+import Constants from 'expo-constants';
+import { useNavigation } from '@react-navigation/native';
+import { Feather as Icon, FontAwesome } from '@expo/vector-icons';
+import { RectButton } from 'react-native-gesture-handler';
+import { Rect } from 'react-native-svg';
 
 const Detail = () => {
+  const navigation = useNavigation();
+
+  function handleNavigateBack() {
+    navigation.goBack();
+  }
+
   return (
-    <View />
+    <SafeAreaView style={{ flex: 1 }}>
+      <View style={styles.container}>
+        <TouchableOpacity onPress={handleNavigateBack}>
+            <Icon name='arrow-left' size={20} color='#34cb79' />
+        </TouchableOpacity>
+
+        <Image style={styles.pointImage} source={{ uri: '' }} />
+
+        <Text style={styles.pointName}>Mercadones</Text>
+        <Text style={styles.pointItems}>Budegas coletadas</Text>
+
+        <View style={styles.address}>
+          <Text style={styles.addressTitle}>Endereço</Text>
+          <Text style={styles.addressContent}>Bauruzera, SP</Text>
+        </View>
+      </View>
+
+      <View style={styles.footer}>
+        <RectButton style={styles.button} onPress={() => {}}>
+          <FontAwesome name='whatsapp' size={20} color='#FFF' />
+          <Text style={styles.buttonText}>Whatsapp</Text>
+        </RectButton>
+
+        <RectButton style={styles.button} onPress={() => {}}>
+          <Icon name='mail' size={20} color='#FFF' />
+          <Text style={styles.buttonText}>E-mail</Text>
+        </RectButton>
+      </View>
+    </SafeAreaView>
   );
 };
 
@@ -11,7 +50,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     padding: 32,
-    paddingTop: 20,
+    paddingTop: 20 + Constants.statusBarHeight,
   },
 
   pointImage: {
